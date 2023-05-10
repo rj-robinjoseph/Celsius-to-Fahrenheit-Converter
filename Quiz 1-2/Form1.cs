8c84e632e1267ecfbeb75c34cbe1118284e55ae5
Robin Joseph <48469274+rj-robinjoseph@users.noreply.github.com>
2023-05-09 22:36:28 -0400

    Add project files.

---
+++ Quiz 1-2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("   Celsius\t\tFahrenheit");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            for (int celsius = 0; celsius <= 20; celsius++)
+            {
+                double fahrenheit = (9.0 / 5.0) * celsius + 32;
+                listBox1.Items.Add("     " + celsius + "°C\t\n="+"\t" + fahrenheit + "  °F");
+
+            }
+        }
+    }
+}
